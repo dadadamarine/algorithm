45513eb2db36d0fd4e0d017fd49fdc1a377e73c8
dadadamarine <dadadamarine@gmail.com>
2019-03-27 14:11:58 +0900

    feature : B10953추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        while (numbers[0] + numbers[1] != 0) {
+        int number = Integer.parseInt(br.readLine());
+        for (int i= 1; i<=number; i++){
+            int[] numbers = Arrays.stream(br.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
             bw.write(add(numbers) + "\n");
-            numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
         bw.close();
     }
