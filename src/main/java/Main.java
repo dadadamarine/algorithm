e6a0bca4817bceb46884fd7e1da4d2bef0fa1709
dadadamarine <dadadamarine@gmail.com>
2019-03-28 17:11:35 +0900

    feature : B2935 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
+    private static final int STUDENTS = 30;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] inputNumbers = br.readLine().split(" ");
-
+        String token;
+        int size1 = getSize(br.readLine());
+        token = br.readLine();
+        int size2 = getSize(br.readLine());
+        bw.write(""+calculate(size1,token,size2));
         bw.close();
     }
 
